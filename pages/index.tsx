d28e6439eec2fef1be82bf6b179dddd4771dbf3d
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-09 10:12:28 -0300

    feat: initial files

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tupis</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </>
+  );
+}
