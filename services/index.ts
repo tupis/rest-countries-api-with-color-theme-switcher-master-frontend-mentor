0a009b53d32970d7a65cc403b50187347421e746
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-10 19:35:15 -0300

    feat: create get all countries

--- services/index.ts
+++ services/index.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const APIContry = axios({
+const APIContry = axios.create({
   baseURL: "https://restcountries.com/v2/",
 });
 
 export const CountryServices = {
-  allCountry: async () => {},
+  allCountry: async () => {
+    return await APIContry.get("all").then((res) => res.data);
+  },
   filterCountryBySearch: async () => {},
   filterCountryByRegion: async () => {},
 };
