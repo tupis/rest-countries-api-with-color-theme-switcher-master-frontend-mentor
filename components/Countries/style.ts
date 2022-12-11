c125ef94b7b6cc7473c03282a7ee80594d46ef39
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-11 18:32:31 -0300

    feat: better effect visual

--- components/Countries/style.ts
+++ components/Countries/style.ts
@@ -3,12 +3,11 @@ import { motion } from "framer-motion";
 
 export const Container = styled(motion.section)`
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-around;
   margin: auto;
   width: 100%;
   max-width: ${(props) => props.theme.width.maxWidth};
   flex-wrap: wrap;
-  gap: 50px 15px;
-  padding: 20px 0;
+  gap: 30px 20px;
 `;
